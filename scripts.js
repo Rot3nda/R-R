6ecd27344f336a0d471bbfe57abbe5df68df4719
Rotenda Ramugondo <rotendaramugondo@Rotendas-MacBook-Pro.local>
2025-04-20 16:43:59 +0200

    update

--- scripts.js
+++ scripts.js
@@ -41,3 +41,92 @@ const userProfileOff=()=>{
     items[i].style.width="21rem";
   }
 }
+
+function allOn(){  
+  document.getElementById("l1").style.backgroundColor="green";
+  document.getElementById("l1").style.color="white";
+  document.getElementById("l1").style.boxShadow="1px 1px 1px 1px black";
+  document.getElementById("l2").style.backgroundColor="transparent";
+  document.getElementById("l2").style.color="black";
+  document.getElementById("l2").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l3").style.backgroundColor="transparent";
+  document.getElementById("l3").style.color="black";
+  document.getElementById("l3").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l4").style.backgroundColor="transparent";
+  document.getElementById("l4").style.color="black";
+  document.getElementById("l4").style.boxShadow="0px 0px 0px 0px transparent";
+}
+
+function rentOn(){
+  document.getElementById("l2").style.backgroundColor="green";
+  document.getElementById("l2").style.color="white";
+  document.getElementById("l2").style.boxShadow="1px 1px 1px 1px black";
+  document.getElementById("l1").style.backgroundColor="transparent";
+  document.getElementById("l1").style.color="black";
+  document.getElementById("l1").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l3").style.backgroundColor="transparent";
+  document.getElementById("l3").style.color="black";
+  document.getElementById("l3").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l4").style.backgroundColor="transparent";
+  document.getElementById("l4").style.color="black";
+  document.getElementById("l4").style.boxShadow="0px 0px 0px 0px transparent";
+}
+
+function buyOn(){
+  document.getElementById("l3").style.backgroundColor="green";
+  document.getElementById("l3").style.color="white";
+  document.getElementById("l3").style.boxShadow="1px 1px 1px 1px black";
+  document.getElementById("l1").style.backgroundColor="transparent";
+  document.getElementById("l1").style.color="black";
+  document.getElementById("l1").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l2").style.backgroundColor="transparent";
+  document.getElementById("l2").style.color="black";
+  document.getElementById("l2").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l4").style.backgroundColor="transparent";
+  document.getElementById("l4").style.color="black";
+  document.getElementById("l4").style.boxShadow="0px 0px 0px 0px transparent";
+}
+
+function sellOn(){
+  document.getElementById("l4").style.backgroundColor="green";
+  document.getElementById("l4").style.color="white";
+  document.getElementById("l4").style.boxShadow="1px 1px 1px 1px black";
+  document.getElementById("l1").style.backgroundColor="transparent";
+  document.getElementById("l1").style.color="black";
+  document.getElementById("l1").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l2").style.backgroundColor="transparent";
+  document.getElementById("l2").style.color="black";
+  document.getElementById("l2").style.boxShadow="0px 0px 0px 0px transparent";
+  document.getElementById("l3").style.backgroundColor="transparent";
+  document.getElementById("l3").style.color="black";
+  document.getElementById("l3").style.boxShadow="0px 0px 0px 0px transparent";
+}
+
+function rateLike(){
+    document.getElementById("like").style.borderRadius="100%";
+    document.getElementById("like").style.border="5px solid pink";
+    document.getElementById("Unlike").style.borderRadius="0%";
+    document.getElementById("Unlike").style.border="0px solid transparent";  
+}
+
+function rateUnLike(){
+  document.getElementById("Unlike").style.borderRadius="100%";
+  document.getElementById("Unlike").style.border="5px solid pink";  
+  document.getElementById("like").style.borderRadius="0%";
+  document.getElementById("like").style.border="0px solid transparent";
+}
+
+function notificationsOff(){
+  document.getElementById("notifications").style.display="none";
+  document.getElementById("notifications1").style.backgroundColor="rgb(215, 215, 215)";
+}
+
+function notificationsOn(){
+  document.getElementById("notifications").style.display="block";
+  document.getElementById("notifications1").style.backgroundColor="rgb(255, 108, 54)";
+}
+
+function overNotification(){
+  document.getElementById("notifications1").style.backgroundColor="(215, 215, 215)";
+}
+
